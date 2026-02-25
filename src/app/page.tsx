b1328b010ba35f400d10f18db159d4863c91d561
Sakshi Rathod <sakshirathod383@gmail.com>
2026-02-25 14:27:56 +0530

    root redirect to login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
+import { redirect } from "next/navigation"
+
 export default function Home() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>Barkaluru</h1>
-      <p>Guardian of the Paws</p>
-      <a href="/login">Go to Login</a>
-    </main>
-  )
+  redirect("/login")
 }
